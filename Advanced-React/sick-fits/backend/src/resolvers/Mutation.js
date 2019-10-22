0278047df36f8ddeb03cdd84215f59a7a0647525
murrayrj <richardmurray-clark@hotmail.com>
2019-10-22 21:53:27 +0100

    feat(wesbos): End of module 3 of Advanced React

---
+++ Advanced-React/sick-fits/backend/src/resolvers/Mutation.js
@@ -0,0 +1,15 @@
+const Mutations = {
+  createItem(parent, args, ctx, info) {
+    const item = ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+};
+
+module.exports = Mutations;
